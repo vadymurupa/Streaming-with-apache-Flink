86a5c680ac78a9aaaf198384aca5b3ce2ca25ac8
vadymurupa <vadim28061993@gmail.com>
2023-12-14 17:31:36 +0200

    Json value deserializer, transaction schema, stream job

--- FlinkEcommerce/src/main/java/Flinkcommerce/DataStreamJob.java
+++ FlinkEcommerce/src/main/java/Flinkcommerce/DataStreamJob.java
@@ -18,6 +18,13 @@
 
 package Flinkcommerce;
 
+import Deserializer.JSONValueDeserializationSchema;
+import Dto.Transaction;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class DataStreamJob {
@@ -27,8 +34,19 @@ public class DataStreamJob {
 		// to building Flink applications.
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+		String topic = "financial_transactions";
 
-		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
+		KafkaSource<Transaction> source = KafkaSource.<Transaction>builder()
+				.setBootstrapServers("localhost:9092")
+				.setTopics(topic)
+				.setGroupId("flink-group")
+				.setStartingOffsets(OffsetsInitializer.earliest())
+				.setValueOnlyDeserializer(new JSONValueDeserializationSchema())
+				.build();
+
+		DataStream<Transaction> transactionStream  = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
+		transactionStream.print();
+
+		JobExecutionResult execute = env.execute("Flink API sceleton. ");
 	}
 }
